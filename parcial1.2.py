1b9df8f63365a0d6211f6c93839ea145918add63
matiasp0508 <147548404+matiasp0508@users.noreply.github.com>
2023-10-12 11:48:27 -0300

    Add files via upload

--- parcial1.2.py
+++ parcial1.2.py
@@ -1,45 +1,71 @@
-
-def mostrar_plato_mas_solicitado(platos: dict) -> None:   
+def mostrar_plato_mas_solicitado(platos: dict, pedidos_clientes: dict) -> None:
     pass
 
-def mostrar_el_cliente_mas_pedidos(pedidos_clientes: dict) -> None:
-    pass
 
-def mostrar_pedido_mayor_valor(pedidos_clientes: dict, platos: dict) -> None:
+def mostrar_cliente_con_mas_pedidos(pedidos_clientes: dict, platos: dict) -> None:
+    cliente_mas_pedidos = 0
+    for max_pedidos in pedidos_clientes.values():
+        if len(max_pedidos[1]) > cliente_mas_pedidos:
+            cliente_mas_pedidos = len(max_pedidos[1])
+            cuit_cliente = max_pedidos[0]
+            total_pedidos = max_pedidos[1]
+    
+    total = 0
+    for id_plato in platos:
+        for id_plato in platos[id_plato]:pass
+
+    # pedidos_clientes = { cuit_cliente: { id_pedido: [plato1, plato2...] } }
+    # pedidos_clientes = {
+    #     "123456789": {
+    #         "pedido1": ["plato1", "plato2", "plato3"]
+    #     },
+    #     "376437345": {
+    #         "pedido2": ["plato2", "plato4"]
+    #     },
+    # }
+
+    # platos = { id_plato: { nombre, valor }}
+    # platos = { "plato1": { "nombre": "milanesa", "valor": 6500.00 } }
+
+def mostrar_pedido_con_mayor_valor(pedidos_clientes: dict, platos: dict) -> None:
     maximo_valor: int = 0
     id_pedido_maximo_valor: str = ""
     primer_id: bool = True
     for cuit_cliente in pedidos_clientes:
-        for pedido in pedidos_clientes[cuit_cliente]:
-            valor_total_pedido = 0
+        for id_pedido in pedidos_clientes[cuit_cliente]:
+            valor_total_pedido: int = 0
             for id_plato in pedidos_clientes[cuit_cliente][id_pedido]:
-                valor_plato = platos[id_plato]["precio"]
+                valor_plato = platos[id_plato]["valor"]
                 valor_total_pedido += valor_plato
-                if primer_id:
-                    maximo_valor = valor_total_pedido
-                    id_pedido_maximo_valor = id_pedido
-                    primer_id = False
-                if valor_total_pedido > maximo_valor:
-                    maximo_valor = valor_total_pedido
-                    id_pedido_maximo_valor = id_pedido
-    print("El peido de mayor valor es: ", id_pedido_maximo_valor)
-    print()
+            if primer_id:
+                maximo_valor = valor_total_pedido
+                id_pedido_maximo_valor = id_pedido
+                primer_id = False
+            if valor_total_pedido > maximo_valor:
+                maximo_valor = valor_total_pedido
+                id_pedido_maximo_valor = id_pedido
+    print("El id de pedido con el mayor valor es: ", id_pedido_maximo_valor)
+    print("Su valor total es: ", maximo_valor)
+            
 
-
-def listar_pedidos(pedidos_clientes: dict) -> None:
+def listar_pedidos_por_cuit(pedidos_clientes: dict) -> None:
     cuit_cliente = input("Ingrese el cuit para ver sus pedidos: ")
     for pedido in pedidos_clientes[cuit_cliente]:
-        print(pedido)
+        print(pedido, ": ", pedidos_clientes[cuit_cliente][pedido])
 
-def pedido_valido(string: str, platos:dict) -> bool:
+
+def pedido_valido(string: str, platos: dict) -> bool:
     split = string.split(",")
+    # El formato es válido
     for s in split:
         if not s.isnumeric():
             return False
+    # Hay al menos 1 plato
     if len(split) < 1:
         return False
-    for id_plato in platos:
-        if id_plato not in platos:
+    # Existe el plato
+    for id_ in split:
+        if id_ not in platos:
             return False
     return True
 
@@ -47,66 +73,65 @@ def pedido_valido(string: str, platos:dict) -> bool:
 def agregar_pedido(platos: dict, pedidos_clientes: dict, contador_id_pedidos: list[int]) -> None:
     for plato in platos:
         print(plato, ": ", platos[plato])
-    cuit_cliente = input("Ingrese el cuit del cliente: ")
-    platos_pedido = input("Ingrese los datos del pedido separados por coma (ej. 1, 2, 3)")
-    if not pedido_valido(platos_pedido):
-        while not pedido_valido(platos_pedido):
-            platos_pedido = input("Ingrese los datos del pedido separados por coma (ej. 1, 2, 3)")
-    platos_pedido: list[str] = platos_pedido.split (",")
-    contador_id_pedidos[0] += 1   
-    id_pedido = str(contador_id_pedidos[0]) 
+    cuit_cliente: str = input("Ingrese el cuit del cliente: ")
+    ingreso_pedido: str = input("Ingrese los platos del pedido, separados por coma (ej: 1,2,3)")
+    if not pedido_valido(ingreso_pedido, platos):
+        while not pedido_valido(ingreso_pedido, platos):
+            platos_pedido: str = input("Ingrese los platos del pedido, separados por coma (ej: 1,2,3)")
+    platos_pedido: list[str] = ingreso_pedido.split(",")
+    contador_id_pedidos[0] += 1
+    id_pedido = str(contador_id_pedidos[0])
     if cuit_cliente not in pedidos_clientes:
         pedidos_clientes[cuit_cliente] = {}
     pedidos_clientes[cuit_cliente][id_pedido] = platos_pedido
 
-    print(pedidos_clientes)
 
+def alta_plato(platos: dict, contador_id_platos: list[int]) -> None:
+    seguir_ingresando: str = "Si"
 
-def baja_plato(platos: dict, contador_id_platos: list[str]) -> None:
-    for plato in platos:
-        print(plato, ": ", platos(plato))
-    id_a_eliminar: str = input("Elija que plato desea borrar: ")
-    if id_a_eliminar in platos:
-        platos.pop(id_a_eliminar)
-    else:
-        print("El dato ingresado no existe")
-
-def modificar_plato(platos: dict, contador_id_platos: list[str]) -> None:
-    for plato in platos:
-        print(plato, ": ", platos(plato))
-    id_a_modificar: str = input("Elija el plato a modificar: ")
-    nuevo_nombre: str = input("Elija el nuevo nombre: ")
-    nuevo_precio: int = float(input("Elija el nuevo precio: "))
-    platos[id_a_modificar] = {"nombre": nuevo_nombre, "precio": nuevo_precio }
-    
-
-
-def alta_platos(platos: dict, contador_id_platos: list[str]) -> None:
-    seguir_ingresando = "Si"
     while seguir_ingresando == "Si":
         nombre = input("Nombre del plato: ")
-        precio = float(input("Ingrese precio del plato: "))
+        valor = float(input("Valor del plato: "))
         contador_id_platos[0] += 1
         id_plato = str(contador_id_platos[0])
-        platos[id_plato] = {"nombre": nombre, "precio": precio }
-        seguir_ingresando: str = (input("Desea seguir agregando platos? (Si/No)"))
+        platos[id_plato] = { "nombre": nombre, "valor": valor }
 
-def abm_platos(platos: dict, contador_id_platos: list[str]) -> None:
+        seguir_ingresando: str = input("Seguir ingresando platos? (Si/No)")
+
+
+def modificar_plato(platos: dict) -> None:
+    for plato in platos:
+        print(plato, ": ", platos[plato])
+    id_a_modificar: str = input("Elija el plato que desea modificar: ")
+    nuevo_nombre: str = input("Ingrese el nuevo nombre del plato: ")
+    nuevo_valor: str = input("Ingrese el nuevo valor del plato: ")
+    platos[id_a_modificar] = { "nombre": nuevo_nombre, "valor": nuevo_valor }
+
+
+def baja_plato(platos: dict) -> None:
+    for plato in platos:
+        print(plato, ": ", platos[plato])
+    id_a_borrar: str = input("Elija el plato que desea borrar: ")
+    if id_a_borrar in platos:
+        platos.pop(id_a_borrar)
+
+
+def abm_platos(platos: dict, contador_id_platos: list[int]) -> None:
     continuar = "Si"
     while continuar == "Si":
         print("ABM de platos")
-        print("1. Alta plato")
-        print("2. Modificar plato")
-        print("3. Baja plato")
-        print()
+        print("1) Alta plato")
+        print("2) Baja plato")
+        print("3) Modificacion plato")
         opcion = int(input("Elija una opcion: "))
         if opcion == 1:
-            alta_platos(platos, contador_id_platos)
+            alta_plato(platos, contador_id_platos)
         elif opcion == 2:
-            modificar_plato(platos)
-        elif opcion == 3:
             baja_plato(platos)
-        continuar: str = input("Desea continuar con el ABM? (Si/No): ")
+        elif opcion == 3:
+            modificar_plato(platos)
+        continuar: str = input("Seguir con ABM de platos? (Si/No)")
+
 
 def mostrar_menu(menu: list[str]) -> None:
     for item in menu:
@@ -116,43 +141,49 @@ def mostrar_menu(menu: list[str]) -> None:
 
 def main() -> None:
     menu: dict[str] = {
-       "a": "ABM de platos",
-       "b": "Agregar un pedido",
-       "c": "Listar pedidos por CUIT",
-       "d": "Mostrar pedido con mayor valor",
-       "e": "Mostrar el cliente con más pedidos",
-       "f": "Mostrar el plato más solicitado",
-       "g": "Salir"   
+        "a": "ABM de platos",
+        "b": "Agregar un pedido",
+        "c": "Listar pedidos por CUIT",
+        "d": "Mostrar pedido con mayor valor",
+        "e": "Mostrar el cliente con más pedidos",
+        "f": "Mostrar el plato más solicitado",
     }
 
+    # pedidos_clientes = { cuit_cliente: { id_pedido: [plato1, plato2...] } }
+    # pedidos_clientes = {
+    #     "123456789": {
+    #         "pedido1": ["plato1", "plato2", "plato3"]
+    #     },
+    #     "376437345": {
+    #         "pedido2": ["plato2", "plato4"]
+    #     },
+    # }
+    pedidos_clientes: dict = {}
 
-    # platos = { id_pedido: { "nombre": , "precio":  } }
-    # platos = { "1": { "nombre": "milanesa", "precio": 6500 } }
-    platos: dict = {} 
-    
-    #pedidos_clientes = { cuit_cliente: { id_pedido: [plato1, plato2, .. ]} }
-    pedidos_clientes = {}
+    # platos = { id_pedido: { nombre, valor }}
+    # platos = { "plato1": { "nombre": "milanesa", "valor": 6500.00 } }
+    platos: dict = {}  
 
     contador_id_platos: list[int] = [0]
     contador_id_pedidos: list[int] = [0]
 
-    continuar = "Si"
+    continuar: bool = "Si"
     while continuar == "Si":
         mostrar_menu(menu)
-        opcion = input("Ingrese una opcion: ")
-        
+        opcion: str = input("Ingrese la opcion que desea: ")
         if opcion == "a":
-            abm_platos(platos, contador_id_platos)
+            abm_platos(platos, contador_id_platos)  ## check
         elif opcion == "b":
-            agregar_pedido(platos, pedidos_clientes, contador_id_pedidos)
+            agregar_pedido(platos, pedidos_clientes, contador_id_pedidos)  ## check
         elif opcion == "c":
-            listar_pedidos(pedidos_clientes, platos)
+            listar_pedidos_por_cuit(pedidos_clientes)  ## check
         elif opcion == "d":
-            mostrar_pedido_mayor_valor(pedidos_clientes)
+            mostrar_pedido_con_mayor_valor(pedidos_clientes, platos)
         elif opcion == "e":
-            mostrar_el_cliente_mas_pedidos(pedidos_clientes)
+            mostrar_cliente_con_mas_pedidos(pedidos_clientes)
         elif opcion == "f":
-            mostrar_plato_mas_solicitado(platos)
-        
-        continuar = input("Desea continuar? (Si/No): ")
+            mostrar_plato_mas_solicitado(platos, pedidos_clientes)
+        continuar: str = input("Continuar? (Si/No)")
+
+
 main()
